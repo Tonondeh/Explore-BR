da7c66038c4942a21d3f39b26f322de121e52b7c
Samuel Brehm <samuelbrehm@gmail.com>
2021-08-25 15:14:21 -0300

    Adicionado folder Login com arquivo de Storyboard e View Controller

---
+++ Explore-BR/Explore-BR/Login/LoginVC.swift
@@ -0,0 +1,15 @@
+//
+//  LoginVC.swift
+//  Explore-BR
+//
+//  Created by Samuel Brehm on 25/08/21.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
