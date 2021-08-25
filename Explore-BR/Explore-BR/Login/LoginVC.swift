967796365b6018652d3c02d50d634aedda859c0f
Samuel Brehm <samuelbrehm@gmail.com>
2021-08-25 16:35:07 -0300

    Finalizado e configurado cornerRadius dos botoes

--- Explore-BR/Explore-BR/Login/LoginVC.swift
+++ Explore-BR/Explore-BR/Login/LoginVC.swift
@@ -9,7 +9,18 @@ import UIKit
 
 class LoginVC: UIViewController {
 
+    @IBOutlet weak var loginAppleButton: UIButton!
+    @IBOutlet weak var loginGoogleButton: UIButton!
+    @IBOutlet weak var loginEmailButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.configureUI()
+    }
+    
+    private func configureUI() {
+        self.loginAppleButton.layer.cornerRadius = 3.0
+        self.loginGoogleButton.layer.cornerRadius = 3.0
+        self.loginEmailButton.layer.cornerRadius = 3.0
     }
 }
