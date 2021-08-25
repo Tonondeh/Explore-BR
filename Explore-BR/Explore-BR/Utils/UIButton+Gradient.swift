f2cc90e5d09222663f979f05304375bb33f98adf
Samuel Brehm <samuelbrehm@gmail.com>
2021-08-25 10:04:35 -0300

    Adicionado extesion para classe UIButton para aplicar background gradient

---
+++ Explore-BR/Explore-BR/Utils/UIButton+Gradient.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+extension UIButton {
+    func applyGradient(colors: [CGColor]) {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = colors
+        gradientLayer.startPoint = CGPoint(x: 0.3, y: 0)
+        gradientLayer.endPoint = CGPoint(x: 1, y: 0)
+        gradientLayer.frame = self.bounds
+        self.layer.insertSublayer(gradientLayer, at: 1)
+    }
+}
