385902da66a18a16ccb0f15c76dc7f57e745de54
Samuel Brehm <samuelbrehm@gmail.com>
2021-08-25 10:07:33 -0300

    criado as cores gradient para usar nos backgrounds do Buttons e Headers

---
+++ Explore-BR/Explore-BR/Utils/ColorsGradient.swift
@@ -0,0 +1,7 @@
+import UIKit
+
+let blueDarkButton = UIColor(red: 0/255, green: 0/255, blue: 70/255, alpha: 1).cgColor
+let blueLightButton = UIColor(red: 28/255, green: 181/255, blue: 224/255, alpha: 0.5).cgColor
+
+let blueNavController = UIColor(red: 52/255, green: 148/255, blue: 230/255, alpha: 1).cgColor
+let pinkNavController = UIColor(red: 236/255, green: 110/255, blue: 173/255, alpha: 1).cgColor
