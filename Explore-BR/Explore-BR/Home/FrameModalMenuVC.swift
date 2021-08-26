7f6b1834b87b27cda35a6dc7cadf4855309e87b0
Samuel Brehm <samuelbrehm@gmail.com>
2021-08-26 11:52:50 -0300

    Criado arquivos slide menu modal

---
+++ Explore-BR/Explore-BR/Home/FrameModalMenuVC.swift
@@ -0,0 +1,8 @@
+//
+//  FrameModalMenuVC.swift
+//  Explore-BR
+//
+//  Created by Samuel Brehm on 26/08/21.
+//
+
+import Foundation
---
+++ Explore-BR/Explore-BR/Home/ModalPresentationController.swift
@@ -0,0 +1,8 @@
+//
+//  ModalPresentationController.swift
+//  Explore-BR
+//
+//  Created by Samuel Brehm on 26/08/21.
+//
+
+import Foundation
