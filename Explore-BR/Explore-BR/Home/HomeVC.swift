3ee9c932cb3d8a4d9c4720c3825cb66a5ab99d6d
Samuel Brehm <samuelbrehm@gmail.com>
2021-08-26 14:31:52 -0300

    Adicionado view com text field

--- Explore-BR/Explore-BR/Home/HomeVC.swift
+++ Explore-BR/Explore-BR/Home/HomeVC.swift
@@ -10,10 +10,25 @@ import UIKit
 class HomeVC: UIViewController {
     
     @IBOutlet weak var statusBackgroundView: UIView!
-
+    @IBOutlet weak var searchTextField: UITextField!
+    @IBOutlet weak var wrapSearchBarView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.configureUI()
+    }
+    
+    func configureUI() {
         self.statusBackgroundView.applyGradientInView(colors: [blueDarkButton,blueLightButton])
+        
+        self.wrapSearchBarView.layer.cornerRadius = self.wrapSearchBarView.frame.size.height/2
+        self.wrapSearchBarView.layer.borderColor = UIColor.white.cgColor
+        self.wrapSearchBarView.layer.borderWidth = 1.5
+        self.wrapSearchBarView.layer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.25).cgColor
+        self.wrapSearchBarView.layer.shadowOpacity = 1
+        self.wrapSearchBarView.layer.shadowRadius = 30
+        self.wrapSearchBarView.layer.shadowOffset = CGSize(width: 0, height: 4.0)
+        
     }
 
 }
