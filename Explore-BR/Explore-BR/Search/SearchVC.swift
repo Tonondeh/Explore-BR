0d99de085ad3db6f41452c4830167347f962614c
Lucas Munho <lucas.munho1994@gmail.com>
2021-08-26 00:15:58 -0300

    atualizacao da tela com o background gradient

--- Explore-BR/Explore-BR/Search/SearchVC.swift
+++ Explore-BR/Explore-BR/Search/SearchVC.swift
@@ -11,10 +11,34 @@ class SearchVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.configNavigationBar()
 
-        // Do any additional setup after loading the view.
+        
     }
     
+    
+    func configNavigationBar(){
+        let layer0 = CAGradientLayer()
+
+                
+
+                layer0.colors = [
+
+                    
+
+                    UIColor(red: 0.204, green: 0.58, blue: 0.902, alpha: 1).cgColor,
+
+                    
+
+                    UIColor(red: 0.925, green: 0.431, blue: 0.678, alpha: 1).cgColor
+
+                    
+
+                ]
+        if let colors = layer0.colors {
+            self.navigationController?.navigationBar.setGradientBackground(colors: colors)
+        }
+    }
 
     /*
     // MARK: - Navigation
