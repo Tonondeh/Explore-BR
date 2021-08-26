5b267b16f6f55a531576f595c5dcd965b893e516
Lucas Munho <lucas.munho1994@gmail.com>
2021-08-26 00:42:59 -0300

    adicionei buttons com as constraints setadas

--- Explore-BR/Explore-BR/Profile/ProfileViewController.swift
+++ Explore-BR/Explore-BR/Profile/ProfileViewController.swift
@@ -93,37 +93,3 @@ class ProfileViewController: UIViewController {
     }
     
 }
-
-//extension UINavigationBar {
-//    func setGradientBackground(colors: [Any]) {
-//        let gradient: CAGradientLayer = CAGradientLayer()
-//        
-//        gradient.locations = [0.2, 1]
-//        
-//        gradient.startPoint = CGPoint(x: 0.25, y: 0.5)
-//        
-//        gradient.endPoint = CGPoint(x: 0.75, y: 0.5)
-//        
-//        var updatedFrame = self.bounds
-//        
-//        updatedFrame.size.height += self.frame.origin.y
-//        
-//        gradient.frame = updatedFrame
-//        
-//        gradient.colors = colors;
-//        
-//        self.setBackgroundImage(self.image(fromLayer: gradient), for: .default)
-//    }
-//    
-//    func image(fromLayer layer: CALayer) -> UIImage {
-//        UIGraphicsBeginImageContext(layer.frame.size)
-//        
-//        layer.render(in: UIGraphicsGetCurrentContext()!)
-//        
-//        let outputImage = UIGraphicsGetImageFromCurrentImageContext()
-//        
-//        UIGraphicsEndImageContext()
-//        
-//        return outputImage!
-//    }
-//}
--- Explore-BR/Explore-BR/Search/SearchVC.swift
+++ Explore-BR/Explore-BR/Search/SearchVC.swift
@@ -8,46 +8,38 @@
 import UIKit
 
 class SearchVC: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.configNavigationBar()
-
+        
         
     }
     
     
     func configNavigationBar(){
         let layer0 = CAGradientLayer()
-
-                
-
-                layer0.colors = [
-
-                    
-
-                    UIColor(red: 0.204, green: 0.58, blue: 0.902, alpha: 1).cgColor,
-
-                    
-
-                    UIColor(red: 0.925, green: 0.431, blue: 0.678, alpha: 1).cgColor
-
-                    
-
-                ]
+        
+        
+        
+        layer0.colors = [
+            
+            
+            
+            UIColor(red: 0.204, green: 0.58, blue: 0.902, alpha: 1).cgColor,
+            
+            
+            
+            UIColor(red: 0.925, green: 0.431, blue: 0.678, alpha: 1).cgColor
+            
+            
+            
+        ]
         if let colors = layer0.colors {
             self.navigationController?.navigationBar.setGradientBackground(colors: colors)
         }
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
+    
+    
 }
