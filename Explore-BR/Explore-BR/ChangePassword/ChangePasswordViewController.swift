f13ef5f2f4485dc5da8ef51ed421c5439f2e28ca
Paulo Ãnes de Souza Rodrigues <pauloadesr@hotmail.com>
2021-08-26 12:12:29 -0300

    colocando imagens no textfield

--- Explore-BR/Explore-BR/ChangePassword/ChangePasswordViewController.swift
+++ Explore-BR/Explore-BR/ChangePassword/ChangePasswordViewController.swift
@@ -9,9 +9,29 @@ import UIKit
 
 class ChangePasswordViewController: UIViewController {
 
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var currentPasswordTextField: UITextField!
+    @IBOutlet weak var newPasswordTextField: UITextField!
+    @IBOutlet weak var confirmPasswordTextField: UITextField!
+    @IBOutlet weak var saveButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        self.configTextField()
     }
 
+    @IBAction func tappedSaveButton(_ sender: UIButton) {
+    }
+    
+    private func configTextField() {
+        var iconImageView = UIImageView()
+        var image = UIImage(named: "lock.png")
+        
+        iconImageView.image = image
+        
+        self.currentPasswordTextField.leftView = iconImageView
+    }
+    
 }
