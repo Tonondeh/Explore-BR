762285d2984d93c1964d8f4ffbc712e30bf4f0be
Paulo Ãnes de Souza Rodrigues <pauloadesr@hotmail.com>
2021-08-26 20:21:42 -0300

    Change password extension and icon

---
+++ Explore-BR/Explore-BR/Utils/TextFieldIconAndImage.swift
@@ -0,0 +1,26 @@
+//
+//  TextFieldIconAndImage.swift
+//  Explore-BR
+//
+//  Created by Paulo Rodrigues on 26/08/21.
+//
+
+import UIKit
+
+extension UITextField {
+
+    
+    func setTextIconAndPlaceholder(icon: UIImage, placeholder: String) {
+        let imageView = UIImageView()
+        imageView.image = icon
+        imageView.frame = CGRect(x: 24, y: -3, width: 21, height: 31)
+        let view = UIView()
+        view.frame = CGRect(x: 0, y: 0, width: 60, height: 25)
+        view.backgroundColor = UIColor.clear
+        view.addSubview(imageView)
+        self.leftView = view
+        self.leftViewMode = UITextField.ViewMode.always
+        self.placeholder = placeholder
+    }
+
+}
