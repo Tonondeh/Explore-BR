08efe9c1f0f0b37943a46bbaf4393aa22545b02b
Lucas Munho <lucas.munho1994@gmail.com>
2021-08-29 20:41:52 -0300

    criacao de tela novo evento e adicionado label e mapkit

---
+++ Explore-BR/Explore-BR/NewEvent/NewEventViewController.swift
@@ -0,0 +1,29 @@
+//
+//  NewEventViewController.swift
+//  Explore-BR
+//
+//  Created by Lucas Munho on 29/08/21.
+//
+
+import UIKit
+
+class NewEventViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
