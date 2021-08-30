f65428b4ac3cef0020260bfcce995c71668250d9
Lucas Munho <lucas.munho1994@gmail.com>
2021-08-29 21:10:46 -0300

    adicionado gradient na parte superior e importado o mapkit

--- Explore-BR/Explore-BR/NewEvent/NewEventViewController.swift
+++ Explore-BR/Explore-BR/NewEvent/NewEventViewController.swift
@@ -6,24 +6,67 @@
 //
 
 import UIKit
+import MapKit
 
 class NewEventViewController: UIViewController {
-
+    
+    @IBOutlet weak var eventLocalLabel: UILabel!
+    
+    
+    @IBOutlet weak var mapKitView: MKMapView!
+    
+    
+    @IBOutlet weak var updateLocationButton: UIButton!
+    
+    
+    @IBOutlet weak var askLabel: UILabel!
+    
+    
+    @IBOutlet weak var addressLabel: UILabel!
+    
+    
+    @IBOutlet weak var eventNameTextField: UITextField!
+    
+    
+    @IBOutlet weak var inicialDateTextField: UITextField!
+    
+    
+    @IBOutlet weak var endDateTextField: UITextField!
+    
+    
+    @IBOutlet weak var descriptionTextField: UITextField!
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.configNavigationBar()
 
-        // Do any additional setup after loading the view.
+       
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func configNavigationBar() {
+        
+        let layer0 = CAGradientLayer()
+        
+        layer0.colors = [
+            
+            UIColor(red: 0.204, green: 0.58, blue: 0.902, alpha: 1).cgColor,
+            
+            UIColor(red: 0.925, green: 0.431, blue: 0.678, alpha: 1).cgColor
+            
+        ]
+        
+        if let colors = layer0.colors {
+            self.navigationController?.navigationBar.setGradientBackground(colors: colors)
+        }
+        
+        
+        let textAttributes = [NSAttributedString.Key.foregroundColor:UIColor.white]
+        navigationController?.navigationBar.titleTextAttributes = textAttributes
+        
     }
-    */
 
 }
