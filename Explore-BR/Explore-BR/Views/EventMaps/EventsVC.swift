924290775502c417b9b218a1c9c3ed549749ba49
eduardoSilvestre <eduardo_mizael@outlook.com.br>
2021-09-27 23:40:37 -0300

    Setando localizacao

--- Explore-BR/Explore-BR/Views/EventMaps/EventsVC.swift
+++ Explore-BR/Explore-BR/Views/EventMaps/EventsVC.swift
@@ -6,44 +6,54 @@
 //
 
 import UIKit
+import MapKit
 import FloatingPanel
 
 class EventsVC: UIViewController{
     
     var fpc: FloatingPanelController?
     
-    @IBOutlet weak var statusBackgroundView: UIView!
+    @IBOutlet weak var mapView: MKMapView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.configFloatingPanel()
         self.configFloatingPanelUI()
+        self.location()
     }
     
     private func configFloatingPanel() {
         
         fpc = FloatingPanelController()
-        
-//        fpc?.delegate = self // Optional
-        
-        // Set a content view controller.
         let eventMapPanelViewController = EventMapPanelViewController()
-        
         fpc?.set(contentViewController: eventMapPanelViewController)
-        
-        // Add and show the views managed by the `FloatingPanelController` object to self.view.
         fpc?.addPanel(toParent: self)
     }
     
     private func configFloatingPanelUI() {
         let appearance = SurfaceAppearance()
-        
         appearance.cornerRadius = 18.0
-        
         fpc?.surfaceView.appearance = appearance
-        
         fpc?.surfaceView.grabberHandlePadding = 10.0
         fpc?.surfaceView.grabberHandleSize = .init(width: 56.0, height: 2.0)
     }
     
+    private func location(){
+        let appleHQ = CLLocation(latitude: 37.334, longitude: -122.008889)
+        let regionRadius: CLLocationDistance = 1000.0
+        let region = MKCoordinateRegion(center: appleHQ.coordinate, latitudinalMeters: regionRadius, longitudinalMeters: regionRadius)
+        mapView.setRegion(region, animated: true)
+        mapView.delegate = self
+        mapView.mapType = .standard
+    }
+    
+}
+
+extension EventsVC: MKMapViewDelegate{
+    
+    func mapViewWillStartRenderingMap(_ mapView: MKMapView) {
+        print("rendering...")
+    }
 }
 
