f98cd944dad7be18bba26d8e8753eb0b3605b413
Samuel Brehm <samuelbrehm@gmail.com>
2021-09-28 16:35:43 -0300

    Criado estrutura tela de novo evento

--- Explore-BR/Explore-BR/Views/NewEvent/NewEventViewController.swift
+++ Explore-BR/Explore-BR/Views/NewEvent/NewEventViewController.swift
@@ -9,16 +9,18 @@ import UIKit
 import MapKit
 
 class NewEventViewController: UIViewController {
-    
-    @IBOutlet weak var eventLocalLabel: UILabel!
-    @IBOutlet weak var mapKitView: MKMapView!
-    @IBOutlet weak var updateLocationButton: UIButton!
-    @IBOutlet weak var askLabel: UILabel!
+        
+    @IBOutlet weak var eventMapView: MKMapView!
+    @IBOutlet weak var updateLocationView: UIView!
     @IBOutlet weak var addressLabel: UILabel!
     @IBOutlet weak var eventNameTextField: UITextField!
     @IBOutlet weak var inicialDateTextField: UITextField!
     @IBOutlet weak var endDateTextField: UITextField!
-    @IBOutlet weak var descriptionTextField: UITextField!
+    @IBOutlet weak var descriptionTextView: UITextView!
+    @IBOutlet weak var photoEventImageView: UIImageView!
+    @IBOutlet weak var selectPhotoView: UIView!
+    @IBOutlet weak var takePhotoCameraView: UIView!
+    @IBOutlet weak var createEventButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,15 +33,9 @@ class NewEventViewController: UIViewController {
         self.navigationController?.navigationBar.tintColor = UIColor.white
     }
     
+    
+    
     private func configNavigationBar() {
-//        let layer0 = CAGradientLayer()
-//        layer0.colors = [
-//            UIColor(red: 0.204, green: 0.58, blue: 0.902, alpha: 1).cgColor,
-//            UIColor(red: 0.925, green: 0.431, blue: 0.678, alpha: 1).cgColor
-//        ]
-//        if let colors = layer0.colors {
-//            self.navigationController?.navigationBar.setGradientBackground(colors: colors)
-//        }
         self.navigationController?.navigationBar.setGradientBackground(colors: [blueNavController, pinkNavController])
         let textAttributes = [NSAttributedString.Key.foregroundColor:UIColor.white]
         navigationController?.navigationBar.titleTextAttributes = textAttributes
@@ -56,8 +52,8 @@ class NewEventViewController: UIViewController {
         self.endDateTextField.layer.borderWidth = 1
         self.endDateTextField.layer.borderColor = UIColor.systemGray.cgColor
         
-        self.descriptionTextField.layer.borderWidth = 1
-        self.descriptionTextField.layer.borderColor = UIColor.systemGray.cgColor
+        self.descriptionTextView.layer.borderWidth = 1
+        self.descriptionTextView.layer.borderColor = UIColor.systemGray.cgColor
         
         if let imageIcon: UIImage = UIImage(named: "nameEventImage.png") {
             
@@ -73,10 +69,10 @@ class NewEventViewController: UIViewController {
             
         }
         
-        if let imageDescripiton: UIImage = UIImage(named: "descriptionImage.png"){
-            
-            self.descriptionTextField.setTextIconAndPlaceholder(icon: imageDescripiton, placeholder: "Dê uma breve descrição para o evento")
-        }
+//        if let imageDescripiton: UIImage = UIImage(named: "descriptionImage.png"){
+//            
+//            self.descriptionTextView.setTextIconAndPlaceholder(icon: imageDescripiton, placeholder: "Dê uma breve descrição para o evento")
+//        }
     }
 
 }
