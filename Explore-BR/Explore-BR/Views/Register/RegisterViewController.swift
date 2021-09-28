f2c485979fbfcda10d8a3108ff6d71fbf455049d
Paulo Ãnes de Souza Rodrigues <pauloadesr@hotmail.com>
2021-09-28 00:43:55 -0300

    validando textFields e button tela register

--- Explore-BR/Explore-BR/Views/Register/RegisterViewController.swift
+++ Explore-BR/Explore-BR/Views/Register/RegisterViewController.swift
@@ -18,7 +18,8 @@ class RegisterViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.configUI()
-   
+        self.checkButtonEnabled(false)
+        self.configTextField()
     }
     
     @IBAction func tappedRegisterButton(_ sender: UIButton) {
@@ -31,7 +32,68 @@ class RegisterViewController: UIViewController {
        
 
     }
+    
+    func configTextFieldDelegates(delegate: UITextFieldDelegate?) {
+        self.nameTextField.delegate = delegate
+        self.emailTextField.delegate = delegate
+        self.passwordTextField.delegate = delegate
+        self.confirmPasswordTextField.delegate = delegate
+    }
+    
+    private func configTextField() {
+        self.configTextFieldDelegates(delegate: self)
+    }
+    
+    func validateTextFields() {
+        let name: String = self.nameTextField.text ?? ""
+        let email: String = self.emailTextField.text ?? ""
+        let password: String = self.passwordTextField.text ?? ""
+        let confirmPassword: String = self.confirmPasswordTextField.text ?? ""
+        
+        if email.isEmpty || password.isEmpty || name.isEmpty || confirmPassword.isEmpty {
+            self.checkButtonEnabled(false)
+        } else {
+            self.checkButtonEnabled(true)
+        }
+    }
+    
+    func checkButtonEnabled(_ isButtonEnabled: Bool) {
+        if isButtonEnabled {
+            self.registerButton.setTitleColor(.white, for: .normal)
+            self.registerButton.isEnabled = true
+        } else {
+            self.registerButton.setTitleColor(.lightGray, for: .normal)
+            self.registerButton.isEnabled = false
+        }
+    }
 
 }
 
+extension RegisterViewController: UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == self.nameTextField {
+            self.emailTextField.becomeFirstResponder()
+        }
+        
+        if textField == self.emailTextField {
+            self.passwordTextField.becomeFirstResponder()
+        }
+        
+        if textField == self.passwordTextField {
+            self.confirmPasswordTextField.becomeFirstResponder()
+        }
+        
+        if textField == self.confirmPasswordTextField {
+            textField.resignFirstResponder()
+        }
+        
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        self.validateTextFields()
+    }
+    
+}
 
