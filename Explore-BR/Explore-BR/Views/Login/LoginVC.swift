6262a8a6af4c89c3372efb4db820cce000fa8660
Andre <tonondeh87@gmail.com>
2021-09-28 18:54:12 -0300

    implacao de login apple - botao

--- Explore-BR/Explore-BR/Views/Login/LoginVC.swift
+++ Explore-BR/Explore-BR/Views/Login/LoginVC.swift
@@ -8,6 +8,8 @@
 import GoogleSignIn
 import UIKit
 import Firebase
+import AuthenticationServices
+import CryptoKit
 
 class LoginVC: UIViewController {
     
@@ -27,8 +29,11 @@ class LoginVC: UIViewController {
     }
     
     @IBAction func tappedLoginAppleButton(_ sender: UIButton) {
+        print("Apertou Botao Apple")
+        startSignInWithAppleFlow()
     }
     
+  
     @IBAction func tappedLoginGoogleButton(_ sender: UIButton) {
         
         guard let clientID = FirebaseApp.app()?.options.clientID else { return }
@@ -73,9 +78,97 @@ class LoginVC: UIViewController {
         self.performSegue(withIdentifier: "SignIn", sender: nil)
     }
     
+    func startSignInWithAppleFlow() {
+      let nonce = randomNonceString()
+      currentNonce = nonce
+      let appleIDProvider = ASAuthorizationAppleIDProvider()
+      let request = appleIDProvider.createRequest()
+      request.requestedScopes = [.fullName, .email]
+      request.nonce = sha256(nonce)
+
+      let authorizationController = ASAuthorizationController(authorizationRequests: [request])
+      authorizationController.delegate = self
+      authorizationController.performRequests()
+    }
+    
     private func configureUI() {
         self.loginAppleButton.layer.cornerRadius = 3.0
         self.loginGoogleButton.layer.cornerRadius = 3.0
         self.loginEmailButton.layer.cornerRadius = 3.0
     }
 }
+
+
+private func randomNonceString(length: Int = 32) -> String {
+  precondition(length > 0)
+  let charset: Array<Character> =
+      Array("0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._")
+  var result = ""
+  var remainingLength = length
+
+  while remainingLength > 0 {
+    let randoms: [UInt8] = (0 ..< 16).map { _ in
+      var random: UInt8 = 0
+      let errorCode = SecRandomCopyBytes(kSecRandomDefault, 1, &random)
+      if errorCode != errSecSuccess {
+        fatalError("Unable to generate nonce. SecRandomCopyBytes failed with OSStatus \(errorCode)")
+      }
+      return random
+    }
+
+    randoms.forEach { random in
+      if remainingLength == 0 {
+        return
+      }
+
+      if random < charset.count {
+        result.append(charset[Int(random)])
+        remainingLength -= 1
+      }
+    }
+  }
+
+  return result
+}
+
+
+// Unhashed nonce.
+fileprivate var currentNonce: String?
+
+private func sha256(_ input: String) -> String {
+  let inputData = Data(input.utf8)
+  let hashedData = SHA256.hash(data: inputData)
+  let hashString = hashedData.compactMap {
+    return String(format: "%02x", $0)
+  }.joined()
+
+  return hashString
+}
+
+
+
+extension LoginVC:ASAuthorizationControllerDelegate {
+    
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithAuthorization authorization: ASAuthorization) {
+      if let appleIDCredential = authorization.credential as? ASAuthorizationAppleIDCredential {
+        guard let _ = currentNonce else {
+          fatalError("Invalid state: A login callback was received, but no login request was sent.")
+        }
+        guard let appleIDToken = appleIDCredential.identityToken else {
+          print("Unable to fetch identity token")
+          return
+        }
+        guard let _ = String(data: appleIDToken, encoding: .utf8) else {
+          print("Unable to serialize token string from data: \(appleIDToken.debugDescription)")
+          return
+        }
+      }
+    }
+
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithError error: Error) {
+      // Handle error.
+      print("==================Sign in with Apple errored: \(error.localizedDescription)")
+    }
+
+  }
+
