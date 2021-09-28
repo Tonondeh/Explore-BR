55168b6b5c7c1a0fd8a664ca4854fbf027b07759
Samuel Brehm <samuelbrehm@gmail.com>
2021-09-28 14:08:34 -0300

    Configuração navigationBar e title

--- Explore-BR/Explore-BR/Views/NewEvent/NewEventViewController.swift
+++ Explore-BR/Explore-BR/Views/NewEvent/NewEventViewController.swift
@@ -11,63 +11,39 @@ import MapKit
 class NewEventViewController: UIViewController {
     
     @IBOutlet weak var eventLocalLabel: UILabel!
-    
-    
     @IBOutlet weak var mapKitView: MKMapView!
-    
-    
     @IBOutlet weak var updateLocationButton: UIButton!
-    
-    
     @IBOutlet weak var askLabel: UILabel!
-    
-    
     @IBOutlet weak var addressLabel: UILabel!
-    
-    
     @IBOutlet weak var eventNameTextField: UITextField!
-    
-    
     @IBOutlet weak var inicialDateTextField: UITextField!
-    
-    
     @IBOutlet weak var endDateTextField: UITextField!
-    
-    
     @IBOutlet weak var descriptionTextField: UITextField!
-    
-    
-    
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.configNavigationBar()
         self.configTextField()
-
-       
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        self.navigationController?.setNavigationBarHidden(false, animated: true)
+        self.navigationController?.navigationBar.tintColor = UIColor.white
     }
     
     private func configNavigationBar() {
-        
-        let layer0 = CAGradientLayer()
-        
-        layer0.colors = [
-            
-            UIColor(red: 0.204, green: 0.58, blue: 0.902, alpha: 1).cgColor,
-            
-            UIColor(red: 0.925, green: 0.431, blue: 0.678, alpha: 1).cgColor
-            
-        ]
-        
-        if let colors = layer0.colors {
-            self.navigationController?.navigationBar.setGradientBackground(colors: colors)
-        }
-        
-        
+//        let layer0 = CAGradientLayer()
+//        layer0.colors = [
+//            UIColor(red: 0.204, green: 0.58, blue: 0.902, alpha: 1).cgColor,
+//            UIColor(red: 0.925, green: 0.431, blue: 0.678, alpha: 1).cgColor
+//        ]
+//        if let colors = layer0.colors {
+//            self.navigationController?.navigationBar.setGradientBackground(colors: colors)
+//        }
+        self.navigationController?.navigationBar.setGradientBackground(colors: [blueNavController, pinkNavController])
         let textAttributes = [NSAttributedString.Key.foregroundColor:UIColor.white]
         navigationController?.navigationBar.titleTextAttributes = textAttributes
-        
+        self.navigationController?.title = "Novo Evento"
     }
     
     private func configTextField() {
