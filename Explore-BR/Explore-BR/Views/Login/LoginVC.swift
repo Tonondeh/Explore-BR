97ecb00950a49dbe526ec5302594563a69e2a530
Andre <tonondeh87@gmail.com>
2021-09-27 23:40:38 -0300

    ajuste de botao google e email

--- Explore-BR/Explore-BR/Views/Login/LoginVC.swift
+++ Explore-BR/Explore-BR/Views/Login/LoginVC.swift
@@ -5,17 +5,66 @@
 //  Created by Samuel Brehm on 25/08/21.
 //
 
+import GoogleSignIn
 import UIKit
+import Firebase
 
 class LoginVC: UIViewController {
-
+    
     @IBOutlet weak var loginAppleButton: UIButton!
     @IBOutlet weak var loginGoogleButton: UIButton!
     @IBOutlet weak var loginEmailButton: UIButton!
     
+    var auth: Auth?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.configureUI()
+        
+        self.auth = Auth.auth()
+    }
+    
+    @IBAction func tappedLoginAppleButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func tappedLoginGoogleButton(_ sender: UIButton) {
+        
+        guard let clientID = FirebaseApp.app()?.options.clientID else { return }
+        
+        // Create Google Sign In configuration object.
+        let config = GIDConfiguration(clientID: clientID)
+        
+        // Start the sign in flow!
+        GIDSignIn.sharedInstance.signIn(with: config, presenting: self) { [unowned self] user, error in
+            
+            if let error = error {
+                return print("google error =", error)
+            }
+            
+            guard let authentication = user?.authentication else { return }
+            guard let idToken = authentication.idToken else { return }
+            
+            let credential = GoogleAuthProvider.credential(withIDToken: idToken,
+                                                           accessToken: authentication.accessToken)
+            
+            self.auth?.signIn(with: credential, completion: { result, error in
+                
+                if let error = error {
+                    print("error =", error)
+                } else {
+                    // fazer navegação para home...
+                }
+                
+            })
+            
+        }
+        
+    }
+    
+    
+    @IBAction func tappedLoginEmailButton(_ sender: UIButton) {
+        print("Chegou")
+        self.performSegue(withIdentifier: "SignIn", sender: nil)
     }
     
     private func configureUI() {
@@ -23,12 +72,4 @@ class LoginVC: UIViewController {
         self.loginGoogleButton.layer.cornerRadius = 3.0
         self.loginEmailButton.layer.cornerRadius = 3.0
     }
-    
-    
-    @IBAction func tappedContinueWithEmailButton(_ sender: UIButton) {
-        self.performSegue(withIdentifier: "SignIn", sender: nil)
-    }
-    
-    
-    
 }
