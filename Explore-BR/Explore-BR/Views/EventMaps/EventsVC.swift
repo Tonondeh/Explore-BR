093c934debf1a7f5774f64f686eb6e0f1b933175
eduardoSilvestre <eduardo_mizael@outlook.com.br>
2021-09-29 20:24:50 -0300

    Localizao atual

--- Explore-BR/Explore-BR/Views/EventMaps/EventsVC.swift
+++ Explore-BR/Explore-BR/Views/EventMaps/EventsVC.swift
@@ -7,24 +7,31 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 import FloatingPanel
+
 
 class EventsVC: UIViewController{
     
-    var fpc: FloatingPanelController?
-    
     @IBOutlet weak var mapView: MKMapView!
+    
+    var fpc: FloatingPanelController?
+    let locationManager = CLLocationManager()
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.configFloatingPanel()
         self.configFloatingPanelUI()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        locationManager.requestWhenInUseAuthorization()
         self.location()
     }
     
     private func configFloatingPanel() {
-        
         fpc = FloatingPanelController()
         let eventMapPanelViewController = EventMapPanelViewController()
         fpc?.set(contentViewController: eventMapPanelViewController)
@@ -40,20 +47,44 @@ class EventsVC: UIViewController{
     }
     
     private func location(){
-        let appleHQ = CLLocation(latitude: 37.334, longitude: -122.008889)
-        let regionRadius: CLLocationDistance = 1000.0
-        let region = MKCoordinateRegion(center: appleHQ.coordinate, latitudinalMeters: regionRadius, longitudinalMeters: regionRadius)
-        mapView.setRegion(region, animated: true)
-        mapView.delegate = self
-        mapView.mapType = .standard
+        
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestAlwaysAuthorization()
+        locationManager.startUpdatingLocation()
+                
     }
     
 }
 
-extension EventsVC: MKMapViewDelegate{
+extension EventsVC: MKMapViewDelegate, CLLocationManagerDelegate{
     
     func mapViewWillStartRenderingMap(_ mapView: MKMapView) {
         print("rendering...")
     }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]){
+        
+        if let location = locations.first {
+            self.locationManager.stopUpdatingLocation()
+            
+            render(location)
+            }
+    }
+    
+    func render(_ location: CLLocation){
+        
+        let coordinate = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+        let span = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
+        let region = MKCoordinateRegion(center: coordinate, span: span)
+       
+        self.mapView.setRegion(region, animated: true)
+
+        let pin = MKPointAnnotation()
+        pin.coordinate = coordinate
+        self.mapView.addAnnotation(pin)
+
+    }
+    
 }
 
