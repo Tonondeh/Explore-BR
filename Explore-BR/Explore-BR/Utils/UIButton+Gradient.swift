379826d316ab22a53a0b90c2099ac2fb6c155c69
Paulo Ãnes de Souza Rodrigues <pauloadesr@hotmail.com>
2021-10-01 18:25:09 -0300

    ajustando extension de gradiente no UIButton para corrigir bug de gradiente faltando na tela register

--- Explore-BR/Explore-BR/Utils/UIButton+Gradient.swift
+++ Explore-BR/Explore-BR/Utils/UIButton+Gradient.swift
@@ -2,11 +2,12 @@ import UIKit
 
 extension UIButton {
     func applyGradient(colors: [CGColor]) {
+        self.layoutIfNeeded()
         let gradientLayer = CAGradientLayer()
         gradientLayer.colors = colors
         gradientLayer.startPoint = CGPoint(x: 0.3, y: 0)
         gradientLayer.endPoint = CGPoint(x: 1, y: 0)
         gradientLayer.frame = self.bounds
-        self.layer.insertSublayer(gradientLayer, at: 1)
+        self.layer.insertSublayer(gradientLayer, at: 0)
     }
 }
