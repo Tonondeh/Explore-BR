1692a8d10033e6beadd0202b7dfe29cb7dd15edd
Samuel Brehm <samuelbrehm@gmail.com>
2021-11-05 21:47:44 -0300

    Criado model UserRegister

--- Explore-BR/Explore-BR/Features /Register/Model/UserRegister.swift
+++ Explore-BR/Explore-BR/Features /Register/Model/UserRegister.swift
@@ -1,22 +1,25 @@
 //
-//  User.swift
+//  UserRegister.swift
 //  Explore-BR
 //
-//  Created by Samuel Brehm on 22/09/21.
+//  Created by Samuel Brehm on 05/11/21.
 //
 
 import Foundation
+
 import FirebaseFirestoreSwift
 
-struct User:Codable {
+struct UserRegister: Codable {
     let id:String
     let name: String?
     let email: String?
+    let password: String?
     
-    enum CodingKeys:String,CodingKey{
+    enum CodingKeys: String, CodingKey {
         case id
         case name
         case email
+        case password
     }
     
 }
