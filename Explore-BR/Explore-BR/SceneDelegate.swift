d9ace443b61a70d9d07e03c53e230e9ef36a91e7
Samuel Brehm <samuelbrehm@gmail.com>
2021-11-23 09:33:40 -0300

    Alterado scene delegate para verificar se usuário está logado com o singleton AuthManager e efetuar a navegação correta.

--- Explore-BR/Explore-BR/SceneDelegate.swift
+++ Explore-BR/Explore-BR/SceneDelegate.swift
@@ -8,15 +8,27 @@
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-
+    
     var window: UIWindow?
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowScene)
+        
+        if AuthManager.shared.getUserAuthenticated() == nil {
+            let storyboard =  UIStoryboard(name: "Home", bundle: nil)
+            let tabbar: UITabBarController? = (storyboard.instantiateViewController(withIdentifier: "HomeTabBar") as? UITabBarController)
+            
+            self.window?.rootViewController = tabbar
+        } else {
+            let storyboard =  UIStoryboard(name: "Login", bundle: nil)
+            let vc = storyboard.instantiateViewController(withIdentifier: "Login") as? LoginVC ?? UIViewController()
+            let navVC = UINavigationController(rootViewController: vc)
+            self.window?.rootViewController = navVC
+        }
+        
+        self.window?.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
